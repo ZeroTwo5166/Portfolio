f7fda89db8c76c5065b8e776610733e00d0ee366
ZeroTwo5166 <subarna.gurung23@gmail.com>
2025-12-01 09:59:19 +0100

    fixed earth

--- src/components/canvas/Planet.jsx
+++ src/components/canvas/Planet.jsx
@@ -5,7 +5,7 @@ import CanvasLoader from '../Loader'
 
 
 const Planet = () => {
-  const planet = useGLTF('./planet/scene.gltf')
+  const planet = useGLTF('/planet/scene.gltf')
   return (
     <primitive object={planet.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
@@ -13,7 +13,7 @@ const Planet = () => {
 
 
 const Earth = () => {
-  const earth = useGLTF('./earth/scene.gltf')
+  const earth = useGLTF('/earth/scene.gltf')
   return (
     <primitive object={earth.scene} scale={2} position-y={0} rotation-y={0} />
   );
