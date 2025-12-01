6a4670bce0ce791d594fcde4249f075ec564b2fe
ZeroTwo5166 <subarna.gurung23@gmail.com>
2025-12-01 10:05:56 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude:['**/*.JPG']
+  assetsInclude: [
+    "**/*.gltf",
+    "**/*.glb",
+    "**/*.bin",
+    "**/*.jpg",
+    "**/*.jpeg",
+    "**/*.png"
+  ]
 })
