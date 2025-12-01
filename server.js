dcef64f68515aa644e8b1f3ffe0e61d6be54b34f
ZeroTwo5166 <subarna.gurung23@gmail.com>
2025-12-01 09:40:04 +0100

    express

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve public assets (raw gltf, images, models, etc.)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Serve built frontend
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get(/.*/, (_, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server running on ${port}`));
